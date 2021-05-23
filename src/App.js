29d846052038d1e41542c7e2dbc4c0d72a998406
Nikita Burenin <nikiburyak@gmail.com>
2021-05-23 10:46:42 +0300

    FirstHW

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { NewsText } from './NewsText';
+import *as news from './news.json'
 
 function App() {
+  const newsText = news.default;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        {newsText.map((el, i)=>{ 
+         return( <div key = {el.id}>
+            <NewsText key = {i} elements={el}/>
+         </div> )
+        })
+        }
     </div>
   );
 }
